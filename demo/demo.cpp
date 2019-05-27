af00823ce456ecf73b354472d1916ddce599b26e
wxx <614610440@qq.com>
2019-05-27 10:27:40 +0800

    increase compatibility

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -4,14 +4,14 @@
 
 int main()
 {
-    auto pca = new n_pca::NPca();
+    n_pca::NPca *pca = new n_pca::NPca();
     pca->init("../config/config.txt");
     pca->process();
     printf("process finished\n");
     pca->test();
 
     int result = pca->detector("../image/orl_faces/s28/3.pgm");
-    auto label_map = pca->getLabelMap("../config/label.txt");
+    std::map<int, std::string> label_map = pca->getLabelMap("../config/label.txt");
     printf("%d, %s\n", result, label_map[result].c_str());
     return 0;
 }
--- demo/detector_demo.cpp
+++ demo/detector_demo.cpp
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <map>
 
 #include "n_pca/n_pca.h"
 
 int main()
 {
-    auto pca = new n_pca::NPca();
+    n_pca::NPca *pca = new n_pca::NPca();
     pca->readData("../data/orl_faces.txt");
     int result = pca->detector("../image/orl_faces/s13/1.pgm");
-    auto label_map = pca->getLabelMap("../config/label.txt");
+    std::map<int, std::string> label_map = pca->getLabelMap("../config/label.txt");
     printf("%d, %s\n", result, label_map[result].c_str());
     return 0;
 }
